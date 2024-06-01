0f127ce6ac59a717fb4340900fa32f262864fd16
Topi Vuori <topi.vuori@tuni.fi>
2024-06-01 15:37:35 +0300

    pins added

--- map.js
+++ map.js
@@ -71,6 +71,7 @@ window.onload = function () {
         var p2 = ctx.transformedPoint(canvas.width, canvas.height);
         ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        drawPoints();
     }
 
     function clamp(value, min, max) {
@@ -79,172 +80,25 @@ window.onload = function () {
 
     var lastX = canvas.width / 2, lastY = canvas.height / 2;
     var dragStart, dragged;
+    // Initialize points directly in the script
+    var points = [
+        { x: 975, y: 420 },
+        { x: 200, y: 250 },
+        { x: 300, y: 350 }
+    ];
 
-    canvas.addEventListener('mousedown', function (evt) {
-        document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragStart = ctx.transformedPoint(lastX, lastY);
-        dragged = false;
-    }, false);
-
-    canvas.addEventListener('mousemove', function (evt) {
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragged = true;
-        if (dragStart) {
-            var pt = ctx.transformedPoint(lastX, lastY);
-            var dx = pt.x - dragStart.x, dy = pt.y - dragStart.y;
-            ctx.translate(dx, dy);
-            redraw();
-        }
-    }, false);
-
-    canvas.addEventListener('mouseup', function (evt) {
-        dragStart = null;
-        if (!dragged) zoom(evt.shiftKey ? -1 : 1);
-        else checkBounds();
-    }, false);
-
-    var scaleFactor = 1.1;
-
-    var zoom = function (clicks) {
-        var pt = ctx.transformedPoint(lastX, lastY);
-        ctx.translate(pt.x, pt.y);
-        var factor = Math.pow(scaleFactor, clicks);
-        var futureScale = ctx.getTransform().a * factor;
-        if (futureScale < 1) {
-            factor = 1 / ctx.getTransform().a;
-        }
-        ctx.scale(factor, factor);
-        ctx.translate(-pt.x, -pt.y);
-        redraw();
-        checkBounds();
-    };
-
-    var handleScroll = function (evt) {
-        var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
-        if (delta) zoom(delta);
-        return evt.preventDefault() && false;
-    };
-
-    canvas.addEventListener('DOMMouseScroll', handleScroll, false);
-    canvas.addEventListener('mousewheel', handleScroll, false);
-
-    function checkBounds() {
-        var transform = ctx.getTransform();
-        var xMin = transform.e;
-        var yMin = transform.f;
-        var xMax = transform.e + canvas.width * transform.a;
-        var yMax = transform.f + canvas.height * transform.d;
-
-        var dx = 0, dy = 0;
-
-        if (xMin > 0) {
-            dx = -xMin;
-        } else if (xMax < canvas.width) {
-            dx = canvas.width - xMax;
-        }
-
-        if (yMin > 0) {
-            dy = -yMin;
-        } else if (yMax < canvas.height) {
-            dy = canvas.height - yMax;
-        }
-
-        if (dx !== 0 || dy !== 0) {
-            ctx.translate(dx, dy);
-            redraw();
-        }
-    }
-};
-// Pin class to manage pin positions and drawing
-class Pin {
-    constructor(x, y, link) {
-        this.x = x;
-        this.y = y;
-        this.link = link;
-    }
-
-    draw(ctx) {
-        var radius = 5;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'red';
-        ctx.fill();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = '#003300';
-        ctx.stroke();
-    }
-
-    contains(mx, my) {
-        var dx = this.x - mx;
-        var dy = this.y - my;
-        return (dx * dx + dy * dy) < 25; // 5^2
-    }
-}
-
-var pins = [];
-// Add a pin (example)
-pins.push(new Pin(100, 100, 'https://example.com'));
-
-function redraw() {
-    var p1 = ctx.transformedPoint(0, 0);
-    var p2 = ctx.transformedPoint(canvas.width, canvas.height);
-    ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    // Draw pins
-    pins.forEach(pin => {
-        pin.draw(ctx);
-    });
-}
-
-// Handle clicks on pins
-canvas.addEventListener('click', function(evt) {
-    var mx = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-    var my = evt.offsetY || (evt.pageY - canvas.offsetTop);
-    var pt = ctx.transformedPoint(mx, my);
-
-    pins.forEach(pin => {
-        if (pin.contains(pt.x, pt.y)) {
-            window.location.href = pin.link;
-        }
-    });
-}, false);
-
-window.onload = function () {
-    var canvas = document.getElementById('mapCanvas');
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    img.src = '/media_muu/aadramin.jpg';
-
-    img.onload = function () {
-        canvas.width = window.innerWidth;
-        canvas.height = img.height * (canvas.width / img.width);
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        trackTransforms(ctx);
-        redraw();
-    };
-
-    function redraw() {
-        var p1 = ctx.transformedPoint(0, 0);
-        var p2 = ctx.transformedPoint(canvas.width, canvas.height);
-        ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-        // Draw pins
-        pins.forEach(pin => {
-            pin.draw(ctx);
+    function drawPoints() {
+        ctx.save();
+        points.forEach(function (point) {
+            ctx.beginPath();
+            ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI, true);
+            ctx.fillStyle = 'red';
+            ctx.fill();
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
         });
+        ctx.restore();
     }
-
-    function clamp(value, min, max) {
-        return Math.max(min, Math.min(max, value));
-    }
-
-    var lastX = canvas.width / 2, lastY = canvas.height / 2;
-    var dragStart, dragged;
 
     canvas.addEventListener('mousedown', function (evt) {
         document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
@@ -261,15 +115,16 @@ window.onload = function () {
         if (dragStart) {
             var pt = ctx.transformedPoint(lastX, lastY);
             var dx = pt.x - dragStart.x, dy = pt.y - dragStart.y;
-            ctx.translate(dx, dy);
-            redraw();
+            if (canTranslate(dx, dy)) {
+                ctx.translate(dx, dy);
+                redraw();
+            }
         }
     }, false);
 
     canvas.addEventListener('mouseup', function (evt) {
         dragStart = null;
         if (!dragged) zoom(evt.shiftKey ? -1 : 1);
-        else checkBounds();
     }, false);
 
     var scaleFactor = 1.1;
@@ -297,6 +152,19 @@ window.onload = function () {
     canvas.addEventListener('DOMMouseScroll', handleScroll, false);
     canvas.addEventListener('mousewheel', handleScroll, false);
 
+    function canTranslate(dx, dy) {
+        var transform = ctx.getTransform();
+        var xMin = transform.e + dx;
+        var yMin = transform.f + dy;
+        var xMax = xMin + canvas.width * transform.a;
+        var yMax = yMin + canvas.height * transform.d;
+
+        var xWithinBounds = (xMin <= 0 && xMax >= canvas.width) || (xMin > 0 && dx < 0) || (xMax < canvas.width && dx > 0);
+        var yWithinBounds = (yMin <= 0 && yMax >= canvas.height) || (yMin > 0 && dy < 0) || (yMax < canvas.height && dy > 0);
+
+        return xWithinBounds && yWithinBounds;
+    }
+
     function checkBounds() {
         var transform = ctx.getTransform();
         var xMin = transform.e;
@@ -318,9 +186,7 @@ window.onload = function () {
             dy = canvas.height - yMax;
         }
 
-        if (dx !== 0 || dy !== 0) {
-            ctx.translate(dx, dy);
-            redraw();
-        }
+        ctx.translate(dx, dy);
+        redraw();
     }
 };
